9fd02418c38b6e2502fae1f41c6d97219b4ff57d
Rafael Delboni <rafadelboni@gmail.com>
2024-01-26 16:57:14 -0300

    feat: adds some mantine components to evaluate

--- src/codes/clj/docs/frontend/core.cljs
+++ src/codes/clj/docs/frontend/core.cljs
@@ -1,18 +1,31 @@
 (ns codes.clj.docs.frontend.core
-  (:require ["@mantine/core" :refer [MantineProvider SimpleGrid]]
+  (:require ["@mantine/core" :refer [MantineProvider ActionIcon SimpleGrid Switch useMantineColorScheme useComputedColorScheme]]
+            ["@tabler/icons-react" :refer [IconSun IconMoon]]
             ["react-dom/client" :as rdom]
             [codes.clj.docs.frontend.infra.helix :refer [defnc]]
             [helix.core :refer [$]]
             [helix.dom :as dom]))
 
+(defnc DarkModeButton []
+  (let [setColorScheme (.-setColorScheme (useMantineColorScheme))
+        computedColorScheme (useComputedColorScheme "light" #js {:getInitialValueInEffect true})]
+    ($ ActionIcon {:onClick #(setColorScheme (if (= computedColorScheme "light") "dark" "light"))
+                   :variant "default"
+                   :size "xl"
+                   :aria-label "Toggle color scheme"}
+       (if (= computedColorScheme "dark")
+         ($ IconSun {:stroke 1.5})
+         ($ IconMoon {:stroke 1.5})))))
+
 (defnc app []
   ($ MantineProvider
      ($ SimpleGrid {:cols 3}
-        (dom/div "1")
+        (dom/div  "1")
         (dom/div "2")
         (dom/div "3")
         (dom/div "4")
-        (dom/div "5")
+        ($ DarkModeButton)
+        ($ Switch {:defaultChecked true :label "I agree to sell my privacy"})
         (dom/div "Helix + Mantine"))))
 
 (defonce root
