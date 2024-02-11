06769d533f460740dd94cc867697f5661e22e57d
Rafael Delboni <rafadelboni@gmail.com>
2024-02-10 21:06:31 -0300

    chore: removes comment code

--- src/codes/clj/docs/frontend/infra/http.cljs
+++ src/codes/clj/docs/frontend/infra/http.cljs
@@ -4,21 +4,3 @@
 
 (defn request! [request-input]
   (http.component/request (:http @system.state/components) request-input))
-
-(comment
-  (-> (http.component/request
-       (http.component/new-http)
-       {:url "https://pokeapi.co/api/v2/pokemon/3/"
-        :method :get})
-      (.then #(prn "then" %))
-      (.catch  #(prn "catch" %)))
-
-  (-> (http.component/request
-       (http.component/new-http-mock {"https://pokeapi.co/api/v2/pokemon/3/"
-                                      {:lag 500
-                                       :status 200
-                                       :body {:poke :fake}}})
-       {:url "https://pokeapi.co/api/v2/pokemon/3/"
-        :method :get})
-      (.then #(prn "then" %))
-      (.catch  #(prn "catch" %))))
