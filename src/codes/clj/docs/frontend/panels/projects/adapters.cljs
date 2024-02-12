e82b2a9c39bfcfe9265b0d0b8685218bc898784c
Rafael Delboni <rafadelboni@gmail.com>
2024-02-12 11:37:09 -0300

    fix: basic list with groups

---
+++ src/codes/clj/docs/frontend/panels/projects/adapters.cljs
@@ -0,0 +1,31 @@
+(ns codes.clj.docs.frontend.panels.projects.adapters 
+  (:require [clojure.string :as str]))
+
+;; TODO unit test all this bellow
+
+(defn grouped-projects->urls
+  [grouped-projects]
+  (->> grouped-projects
+       (map (fn [{:keys [url artifact]}]
+              (str/replace-first url (re-pattern (str "/" artifact "$")) "")))
+       distinct
+       (into #{})))
+
+(defn urls->org-image "Only works with github for now."
+  [urls]
+  (when-let [github (first (filter #(re-matches #".*github\.com.*" %) urls))]
+    (str github ".png?size=50")))
+
+(defn projects->groups
+  [projects]
+  (->> projects
+       (group-by :group)
+       (map (fn [[group projects]]
+              (let [urls (grouped-projects->urls projects)
+                    image (urls->org-image urls)]
+                {:id group
+                 :image image
+                 :count-projects (count projects)
+                 :urls urls
+                 :projects projects})))))
+
--- src/codes/clj/docs/frontend/panels/projects/view.cljs
+++ src/codes/clj/docs/frontend/panels/projects/view.cljs
@@ -1,57 +1,13 @@
 (ns codes.clj.docs.frontend.panels.projects.view
-  (:require ["@mantine/core" :refer [Container Grid Title]]
+  (:require ["@mantine/core" :refer [Accordion Anchor Avatar Container Grid
+                                     Group Text Title]]
             [codes.clj.docs.frontend.infra.helix :refer [defnc]]
+            [codes.clj.docs.frontend.panels.projects.adapters :as adapters]
             [helix.core :refer [$]]
-            [helix.dom :as dom]
-            [clojure.string :as str]))
-
+            [helix.dom :as dom]))
+;
 (def projects-data
-  [{:artifact "core.cache",
-    :group "org.clojure",
-    :id "org.clojure/core.cache",
-    :manifest "deps",
-    :name "org.clojure/core.cache",
-    :paths ["/src/main/clojure"],
-    :sha "b9b3192fd7beda68a06af7de4b6d4c2a54515094",
-    :tag "v1.0.225",
-    :url "https://github.com/clojure/core.cache"}
-   {:artifact "data.xml",
-    :group "org.clojure",
-    :id "org.clojure/data.xml",
-    :manifest "pom",
-    :name "org.clojure/data.xml",
-    :paths ["/src/main/clojure" "/src/main/java" "/src/main/resources"],
-    :sha "4fbff240e0e4d57537b616fc4c2b7f28f6555e20",
-    :tag "v0.2.0-alpha8",
-    :url "https://github.com/clojure/data.xml"}
-   {:artifact "data.csv",
-    :group "org.clojure",
-    :id "org.clojure/data.csv",
-    :manifest "deps",
-    :name "org.clojure/data.csv",
-    :paths ["/src/main/clojure"],
-    :sha "80c94ef6592f07d62c489359e8535343689d8135",
-    :tag "v1.0.1",
-    :url "https://github.com/clojure/data.csv"}
-   {:artifact "core.async",
-    :group "org.clojure",
-    :id "org.clojure/core.async",
-    :manifest "deps",
-    :name "org.clojure/core.async",
-    :paths ["/src/main/clojure"],
-    :sha "96adc333bb02c8fc60bd51306950b3ad291b3460",
-    :tag "v1.6.673",
-    :url "https://github.com/clojure/core.async"}
-   {:artifact "data.json",
-    :group "org.clojure",
-    :id "org.clojure/data.json",
-    :manifest "deps",
-    :name "org.clojure/data.json",
-    :paths ["/src/main/clojure"],
-    :sha "43c122e91c5c5e46dc58dc7e8fcbb64bb9f88a14",
-    :tag "v2.4.0",
-    :url "https://github.com/clojure/data.json"}
-   {:artifact "core.memoize",
+  [{:artifact "core.memoize",
     :group "org.clojure",
     :id "org.clojure/core.memoize",
     :manifest "deps",
@@ -77,31 +33,75 @@
     :paths ["/src/clj" "/src/main/clojure" "/src/main/java" "/src/resources"],
     :sha "ce55092f2b2f5481d25cff6205470c1335760ef6",
     :tag "clojure-1.11.1",
-    :url "https://github.com/clojure/clojure"}])
+    :url "https://github.com/clojure/clojure"}
+   {:artifact "helix",
+    :group "lilactown",
+    :id "lilactown/helix",
+    :manifest "deps",
+    :name "lilactown/helix",
+    :paths ["/src" "/resources"],
+    :sha "35127b79405e5dff6d9b74dfc674280eb93fab6d",
+    :tag "v0.2.0",
+    :url "https://github.com/lilactown/helix"}
+   {:artifact "flex",
+    :group "lilactown",
+    :id "lilactown/flex",
+    :manifest "deps",
+    :name "lilactown/flex",
+    :paths ["/src" "/resources"],
+    :sha "35127b79405e5dff6d9b74dfc674280eb93fab6d",
+    :tag "v0.2.0",
+    :url "https://github.com/lilactown/flex"}
+   {:artifact "dummy",
+    :group "someone",
+    :id "someone/dummy",
+    :manifest "deps",
+    :name "someone/dummy",
+    :paths ["/src" "/resources"],
+    :sha "35127b79405e5dff6d9b74dfc674280eb93fab6d",
+    :tag "v0.0.1",
+    :url "https://gitlab.com/someone/dummy"}])
 
-(defn projects->groups [projects]
-  (->> projects
-       (group-by :group)
-       (map (fn [[group projects]]
-              (let [urls (->> projects
-                              (map (fn [{:keys [url artifact]}]
-                                     (str/replace-first url (re-pattern (str "/" artifact "$")) "")))
-                              distinct
-                              (into #{}))
-                    image (when-let [github (first (filter #(re-matches #".*github\.com.*" %) urls))]
-                            (str github ".png?size=50"))]
-                {:id group
-                 :image image
-                 :count-projects (count projects)
-                 :urls urls
-                 :projects projects})))))
+(defnc accordion-label [{:keys [label image urls count-projects]}]
+  ($ Group {:wrap "nowrap"}
+    (if image
+      ($ Avatar {:src image :radius "xl" :size "lg"})
+      ($ Avatar {:radius "xl" :size "lg"} label))
+    (dom/div
+      ($ Text label)
+      ($ Text {:size "sm" :c "dimmed" :fw 400}
+        (mapv #($ Anchor {:component "a" :inherit true :href %} %) urls)
+        " has " count-projects " indexed projects"))))
 
-(projects->groups projects-data)
+(defnc accordion-item [{:keys [id image count-projects urls projects]}]
+  ($ Accordion.Item {:value id :key id}
+    ($ Accordion.Control
+      ($ accordion-label {:label id
+                          :image image
+                          :urls urls
+                          :count-projects count-projects})
+      ($ Accordion.Panel
+        ($ Text {:size "sm"}
+          (str projects))))))
 
-;; TODO 
 (defnc group-by-orgs []
-  ($ Container {:size "md"}
-    ($ Grid {:id "group-by-orgs"}
-      ($ Grid.Col {:span 12}
-        (dom/section
-          ($ Title {:order 2} "Projects Grouped by Organizations"))))))
+  (let [groups (adapters/projects->groups projects-data)
+        group-item (map (fn [group] ($ accordion-item {:& group})) groups)]
+
+    ($ Container {:size "md"}
+      ($ Grid {:id "group-by-orgs"}
+        ($ Grid.Col {:span 12}
+          (dom/section
+            ($ Title {:order 1}
+              "Projects by "
+              ($ Text {:component "span" :inherit true :variant "gradient"
+                       :gradient #js {:from "cyan" :to "green" :deg 90}}
+                "Organizations"))))
+
+        (dom/div (dom/br))
+
+        ($ Grid.Col {:span 12}
+          ($ Accordion {:defaultValue "org.clojure"
+                        :chevronPosition "right"
+                        :variant "contained"}
+            group-item))))))
